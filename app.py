6722cb9ce357cd2705ae27cc0a83a9d043c320d1
Sandip54-DevOps <sandip.chatterjee.devops@gmail.com>
2023-03-22 17:05:29 +0530

    test code push

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome to Python App!"
+
+@app.route('/name')
+def name():
+    return 'Hi, I am Sandip Chatterjee'
+
+@app.route('/age')
+def age():
+    return '26'
+
+@app.route('/about')
+def about():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
